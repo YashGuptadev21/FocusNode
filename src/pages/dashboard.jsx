9a05d799580cb8a30f8501a21a413430feeaffa7
Yash Gupta <you@example.com>
2024-10-14 21:40:51 +0530

    Initial commit

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,4 +1,14 @@
 import React, { useEffect } from 'react'
+import {
+   MdAdminPanelSettings,
+   MdKeyBoardArrowDown,
+   MdKeyboardArrowUp,
+   MdKeyboardDoubleArrowUp,
+} from "react-icons/md"
+import { LuClipboardEdit } from "react-icons/lu"
+import { FaNewspaper, FaUsers } from "react-icons/fa"
+import { FaArrowToDot } from "react-icons/fa6"
+import moment from "moment"
 
 const DashBoard = () => {  
    return( <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,56 @@
-export function getInitials(fullName){
+export const formatDate = (date) => {
+  const month = date.tolocalString("en-US", { month: "short" });
+  const day = date.getDate();
+  const year = date.getFullYear();
 
-    if(!fullName || typeof fullName !== "string"){
-        return "";
-    }
-    const names = fullName.split(" ");
+  const formattedDate = `${day}-${month}-${year}`;
 
+  return formattedDate;
+};
 
-    const initials = names.slice(0, 2).map((name) => name[0].toUpperCase())
+export function dateFormatter(dateString) {
+  const inputDate = new Date(dateString);
 
-    const initialStr = initials.join("")
+  if (isNaN(inputDate)) {
+    return "Invalid Date";
+  }
 
-    return initialStr
+  const year = inputDate.getFullYear();
+  const month = String(inputDate.getMonth() + 1).padStart(2, "0");
+  const day = String(inputDate.getDate()).padStart(2, "0");
+
+  const formattedDate = `${year}-${month}-${day}`;
+  return formattedDate;
 }
+
+export function getInitials(fullName) {
+  if (!fullName || typeof fullName !== "string") {
+    return "";
+  }
+  const names = fullName.split(" ");
+
+  const initials = names.slice(0, 2).map((name) => name[0].toUpperCase());
+
+  const initialStr = initials.join("");
+
+  return initialStr;
+}
+
+export const PRIORITYSTYLES = {
+  high: "text-red-600",
+  medium: "text-yellow-600",
+  low: "text-blue-600",
+};
+
+export const TASK_TYPE = {
+  todo: "bg-blue-600",
+  "in-progress": "bg-yellow-600",
+  completed: "bg-green-600",
+};
+
+export const BGS = [
+  "bg-blue-600",
+  "bg-yellow-600",
+  "bg-red-600",
+  "bg-green-600",
+];
