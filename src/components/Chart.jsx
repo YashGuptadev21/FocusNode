db20f38ddf7bd80e880487a13cb4592e3007789e
Yash Gupta <you@example.com>
2024-10-15 13:01:22 +0530

    Initial Commit

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -14,14 +14,14 @@ import { chartData } from "../assets/data";
 const Chart = () => {
   return (
     <div>
-      <ResponsiveContainer
-      width={"100%"}
-      height={500}>
-        <BarChart 
-        width={150}
-        height={40}
-        data={chartData}
-        ></BarChart>
+      <ResponsiveContainer width={"100%"} height={500}>
+        <BarChart width={150} height={40} data={chartData}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Legend />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Bar dataKey="total" fill="#8884d8" />
+        </BarChart>
       </ResponsiveContainer>
     </div>
   );
