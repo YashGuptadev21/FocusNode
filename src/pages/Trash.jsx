34d68161ad7631ad603a7c49e70955ecba72bfab
Yash Gupta <you@example.com>
2024-10-23 13:26:06 +0530

    Initial Commit

--- src/pages/Trash.jsx
+++ src/pages/Trash.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  MdDelete,
+  MdKeyboardArrowDown,
+  MdKeyboardArrowUp,
+  MdKeyboardDoubleArrowUp,
+  MdOutlineRestore,
+} from "react-icons/md";
+import Title from "../components/Title";
+import Button from "../components/Button";
+import { TASK_TYPE } from "../utils";
+
+const ICONS = {
+  high: <MdKeyboardDoubleArrowUp />,
+  medium: <MdKeyboardArrowUp />,
+  low: <MdKeyboardArrowDown />,
+};
 
 const Trash = () => {
-  return (
-    <div>Trash</div>
-  )
-}
+  const [openDialog, setOpenDialog] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [msg, setMsg] = useState(null);
+  const [type, setType] = useState("delete");
+  const [selected, setSelected] = useState("");
 
-export default Trash
+  const TableHeader = () => (
+    <thead className="border-b border-gray-300 dark:border-gray-600">
+      <tr className="text-black dark:text-white text-left">
+        <th className="py-2">Task Title</th>
+        <th className="py-2">Priority</th>
+        <th className="py-2">Stage</th>
+        <th className="py-2 line-clamp-1">Modified On</th>
+      </tr>
+    </thead>
+  );
+
+  const TableRow = ({ item }) => (
+    <tr className="border-b border-gray-200 dark:border-gray-700 text-gray-600 dark:text-gray-400 hover:bg-gray-400/10">
+      <td className="py-2">
+        <div className="flex items-center gap-2">
+          <div
+            className={clsx("w-4 h-4 rounded-full", TASK_TYPE[item.stage])}
+          />
+          <p className="w-full line-clamp-2 text-base text-black dark:text-gray-400">
+            {item?.title}
+          </p>
+        </div>
+      </td>
+
+      <td className="py-2 capitalize"></td>
+    </tr>
+  );
+
+  return (
+    <>
+      <div className="w-full md:px-1 px-0 mb-6">
+        <div className="flex items-center justify-between mb-8">
+          <Title title="Deleted Tasks" />
+
+          <div className="flex gap-2 md:gap-4 items-center">
+            <Button
+              label="Restore All"
+              icon={<MdOutlineRestore className="text-lg hidden md:flex" />}
+              className="flex flex-row-reverse gap-1 items-center text-black text-sm md:text-base rounded-md 2xl:py-2.5"
+              onClick={() => restoreAllClick()}
+            />
+            <Button
+              label="Delete All"
+              icon={<MdDelete className="text-lg hidden md:flex" />}
+              className="flex flex-row-reverse gap-1 items-center text-black text-sm md:text-base rounded-md 2xl:py-2.5"
+              onClick={() => deleteAllClick()}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Trash;
