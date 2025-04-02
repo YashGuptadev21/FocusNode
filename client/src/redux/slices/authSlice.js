f4b009bd90c89a5906f69473ac57bc62242ec4a9
Yash Gupta <you@example.com>
2025-04-02 21:09:05 +0530

    Initial Commit

--- client/src/redux/slices/authSlice.js
+++ client/src/redux/slices/authSlice.js
@@ -5,8 +5,10 @@ const initialState = {
   user: localStorage.getItem("user-info")
     ? JSON.parse(localStorage.getItem("user-info"))
     : null,
-
-  isSidebarOpen: false,
+    token : localStorage.getItem("token") || null,
+     isSidebarOpen : localStorage.getItem("isSidebarOpen")
+     ? JSON.parse(localStorage.getItem("isSidebarOpen"))
+     : false, //
 };
 
 const authSlice = createSlice({
@@ -14,12 +16,15 @@ const authSlice = createSlice({
   initialState, 
   reducers: {
     setCredentials: (state, action) => {
-      state.user = action.payload;
+      state.user = action.payload.user;
+      state.token = action.payload.token
       localStorage.setItem("user-info", JSON.stringify(action.payload));
     },
     logout: (state, action) => {
       state.user = null;
+      state.token = null;
       localStorage.removeItem("user-info");
+      localStorage.removeItem("token")
     },
     setOpenSidebar: (state, action) => {
       state.isSidebarOpen = action.payload;
