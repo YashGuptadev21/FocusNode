a928a7f8f7ecd0b4986ca418bcc4c6e09ae70c8e
Yash Gupta <you@example.com>
2025-04-02 21:00:40 +0530

    Initial Commit

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -2,7 +2,14 @@ import { createApi , fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 const API_URI = import.meta.env.VITE_APP_BASE_URL
 
-const baseQuery = fetchBaseQuery({ baseUrl: API_URI + "/api", credentials: "include", mode:"cors" ,headers : ""});
+const baseQuery = fetchBaseQuery({ baseUrl: API_URI + "/api", credentials: "include", mode:"cors" ,prepareHeaders :(headers) =>{
+  if (token) {
+    headers.set("Authorization", `Bearer ${token}`);
+  }
+  
+  headers.set("Content-Type", "application/json");
+  return headers;
+}});
 
 export const apiSlice = createApi({
   baseQuery,
